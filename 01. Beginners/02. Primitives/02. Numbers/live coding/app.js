b77f5f2c8cfc14299a2ab9ccbea8ca399f5c8668
YusuftheCoder <cx5ftcpp89@privaterelay.appleid.com>
2026-08-13 20:06:56 +0000

    Solve 01/02/02: Numbers

---
+++ 01. Beginners/02. Primitives/02. Numbers/live coding/app.js
@@ -0,0 +1,20 @@
+// JavaScript has one number type
+// Positive Number
+// Negative Number
+// Whole Number (integers)
+// Decimal Numbers
+
+const randomNumber = 7;
+const favNumber = 3;
+let num = 2;
+
+// Basic Math
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 ** 2);
+console.log(num++);
+console.log(num--);
+
