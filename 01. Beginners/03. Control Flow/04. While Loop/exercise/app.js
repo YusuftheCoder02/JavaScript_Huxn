6809fdcb0c9d4e9abe6b5df69cb77de1c0ea1e1c
YusuftheCoder <cx5ftcpp89@privaterelay.appleid.com>
2026-08-16 23:27:00 +0000

    Solve 01/03/04: While Loop

---
+++ 01. Beginners/03. Control Flow/04. While Loop/exercise/app.js
@@ -0,0 +1,17 @@
+// Print "Your Name" 100 times to the console using while loop
+// Iteration will start from 10.
+
+let i = 10;
+while (i <= 100) {
+  console.log("Yusuf Utkan", i);
+  i++;
+}
+
+// Course Solution:
+
+let i = 10;
+
+while (i <= 100) {
+  console.log("HuXn WebDev");
+  i++;
+}
