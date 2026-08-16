e8c23d5fd3081c5135e77b95c3239d0bddccddb8
YusuftheCoder <cx5ftcpp89@privaterelay.appleid.com>
2026-08-16 23:21:52 +0000

    Solve 01/03/03: For Loop

---
+++ 01. Beginners/03. Control Flow/03. For Loop/exercise/app.js
@@ -0,0 +1,11 @@
+// Print your name & numbers from 0 through 1000
+
+for (let i = 0; i <= 1000; i++) {
+  console.log("Yusuf Utkan", i);
+}
+
+// Course Solution
+
+for (let i = 0; i <= 1000; i++) {
+  console.log("HuXn WebDev", i);
+}
