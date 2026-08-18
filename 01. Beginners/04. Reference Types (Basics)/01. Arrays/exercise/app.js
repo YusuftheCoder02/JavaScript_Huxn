e5c85035d748335dc6954d6371304df947f999e2
YusuftheCoder <cx5ftcpp89@privaterelay.appleid.com>
2026-08-18 20:38:33 +0000

    Solve 01/04/01: Arrays

---
+++ 01. Beginners/04. Reference Types (Basics)/01. Arrays/exercise/app.js
@@ -0,0 +1,34 @@
+// 1. Create array name (favSingers) Store 3 favorite singers.
+// 2. log the first singer in that array.
+// 3. Create array name (favNumbers) & store 4 fav Numbers.
+// 4. Create array name (mixedArr) store ["string", ["otherarray"], 123, true]
+// 5. Now Access each item in that array by using [] notation.
+
+let favSingers = ["Adele", "Ed Sheeran", "Beyoncé"];
+console.log(favSingers[0]); // Adele
+
+let favNumbers = [7, 14, 21, 28];
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]); // "string"
+console.log(mixedArr[1]); // ["otherarray"]
+console.log(mixedArr[2]); // 123
+console.log(mixedArr[3]); // true
+
+// Course Solution
+
+const favSingers = ["21 Savage", "Post Malone", "NF"];
+console.log(favSingers[0]);
+
+const favNumbers = [321, 123, 1, 23];
+console.log(favNumbers);
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr);
+
+// ACCESSING ITEMS FROM ARRAY
+console.log(favSingers[0]);
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(mixedArr[3]);
